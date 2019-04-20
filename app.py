aaf3581be06e33020d188b8589d315d6cf3d81cc
Yashas Reddy <yashasycsc@outlook.com>
2019-04-21 00:51:24 +0530

    kite login route

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import base64
 import json
 import requests
+import kiteconnect
 
 app = Flask(__name__)
 
@@ -14,10 +15,40 @@ ALGOTRADE_BOT_TOKEN = "878159613:AAFEF_7UtZgkFbaLhsyP0ddlmT1L2m-MjaA"
 ALGOTRADE_BOT_URL = "https://api.telegram.org/bot{}".format(ALGOTRADE_BOT_TOKEN)
 ALGOTRADE_BOT_SEND_URL = ALGOTRADE_BOT_URL+"/sendMessage?chat_id="+TESTING_GROUP_ID+"&text="
 
+# Zerodha Constants
+KITE_API_KEY = "8k89pux7hxe58snm"
+KITE_API_SECRET = "24uqvpxbalc9yc8nmnrb0ei4y9crhvke"
+
 @app.route('/')
 def hello():
-    data = {'message': 'hi there'}
-    return jsonify(data)
+    response = {
+    "server": "Aws lambda",
+    "status": "Working"
+    }
+    return jsonify(response)
+
+@app.route("/kite_login", methods=["GET"])
+def handle_request_token():
+    '''
+    Updates Kite Connect access tokens using obtained request token
+    '''
+    try:
+        # Get url parameter request_token
+        request_token = request.args.get('request_token')
+        login_status = request.args.get('status')
+        response = {
+            "request_token": request_token,
+            "login_status": login_status
+        }
+    
+    except:
+        return jsonify({
+            'error': True,
+            'function': "handle_request_token()",
+            'description': "Updates Kite Connect access tokens using obtained request token"
+        })
+    
+    return jsonify(response)
 
 @app.route("/signal/<string:encoded_data>", methods=["GET"])
 def get_signal_encoded(encoded_data):
@@ -37,7 +68,9 @@ def get_signal_encoded(encoded_data):
 
     except:
         return jsonify({
-            'error': True
+            'error': True,
+            'function': "get_signal_encoded()",
+            'description': "handles encoded signals from amibroker and sends telegram notification"
         })
 
     return jsonify({
@@ -47,4 +80,4 @@ def get_signal_encoded(encoded_data):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
